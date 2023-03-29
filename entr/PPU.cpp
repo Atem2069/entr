3a5120fa56547266931083a161bdbe5932aec146
Henry Southall <hen0104.hs@gmail.com>
2023-03-29 23:18:00 +0100

    implement skeleton code for 2d engine

---
+++ entr/PPU.cpp
@@ -0,0 +1,81 @@
+#include"PPU.h"
+
+PPU::PPU(std::shared_ptr<InterruptManager> interruptManager, std::shared_ptr<Scheduler> scheduler)
+{
+	m_interruptManager = interruptManager;
+	m_scheduler = scheduler;
+
+	memset(m_renderBuffer, 0, 256 * 384 * 8);
+	memset(m_safeDisplayBuffer, 0, 256 * 384 * 4);
+
+	VCOUNT = 0;
+	m_state = PPUState::HDraw;
+
+	//todo: schedule first ppu event here
+}
+
+PPU::~PPU()
+{
+
+}
+
+void PPU::updateDisplayOutput()
+{
+	memcpy(m_safeDisplayBuffer, m_renderBuffer[!pageIdx], 256 * 384 * sizeof(uint32_t));
+}
+
+void PPU::registerMemory(std::shared_ptr<NDSMem> mem)
+{
+	Logger::getInstance()->msg(LoggerSeverity::Info, "Registered memory with PPU!");
+	m_mem = mem;
+}
+
+void PPU::eventHandler()
+{
+	//todo
+}
+
+void PPU::HDraw()
+{
+	//todo
+}
+
+void PPU::HBlank()
+{
+	//todo
+}
+
+void PPU::VBlank()
+{
+	//todo
+}
+
+uint8_t PPU::readIO(uint32_t address)
+{
+	//todo
+}
+
+void PPU::writeIO(uint32_t address, uint8_t value)
+{
+	//todo
+}
+
+uint32_t PPU::col16to32(uint16_t col)
+{
+	int red = (col & 0b0000000000011111);
+	red = (red << 3) | (red >> 2);
+	int green = (col & 0b0000001111100000) >> 5;
+	green = (green << 3) | (green >> 2);
+	int blue = (col & 0b0111110000000000) >> 10;
+	blue = (blue << 3) | (blue >> 2);
+
+	return (red << 24) | (green << 16) | (blue << 8) | 0x000000FF;
+}
+
+void PPU::onSchedulerEvent(void* context)
+{
+	PPU* thisPtr = (PPU*)context;
+	thisPtr->eventHandler();
+}
+
+uint32_t PPU::m_safeDisplayBuffer[256 * 384] = {};
---
+++ entr/PPU.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include"Logger.h"
+#include"NDSMem.h"
+#include"InterruptManager.h"
+#include"Scheduler.h"
+
+enum class PPUState
+{
+	HDraw,
+	HBlank,
+	VBlank
+};
+
+class PPU
+{
+public:
+	PPU(std::shared_ptr<InterruptManager> interruptManager, std::shared_ptr<Scheduler> scheduler);
+	~PPU();
+
+	void updateDisplayOutput();
+
+	void registerMemory(std::shared_ptr<NDSMem> mem);
+
+	uint8_t readIO(uint32_t address);
+	void writeIO(uint32_t address, uint8_t value); 
+
+	static void onSchedulerEvent(void* context);
+
+	static uint32_t m_safeDisplayBuffer[256 * 384];
+private:
+	PPUState m_state = {};
+	bool registered = false;
+	std::shared_ptr<NDSMem> m_mem;
+	std::shared_ptr<InterruptManager> m_interruptManager;
+	std::shared_ptr<Scheduler> m_scheduler;
+
+	uint32_t m_renderBuffer[2][256 * 384];
+	bool pageIdx = false;
+
+	void eventHandler();
+	void HDraw();
+	void HBlank();
+	void VBlank();
+
+	uint32_t col16to32(uint16_t col);
+
+	uint32_t DISPCNT = {};
+	uint16_t DISPSTAT = {};
+	uint16_t VCOUNT = {};
+};
