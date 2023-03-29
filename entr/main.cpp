c8294262b677913f7b4e01ac15d0afb8d71d1068
Henry Southall <hen0104.hs@gmail.com>
2023-03-30 00:30:58 +0100

    move emulation to separate thread

--- entr/main.cpp
+++ entr/main.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
+#include<thread>
 
 #include"NDS.h"
 #include"Logger.h"
+
+std::shared_ptr<NDS> m_nds;
+
+void emuWorkerThread();
+
 
 int main()
 {
 	Logger::getInstance()->msg(LoggerSeverity::Info, "Hello world!");
 
-	NDS m_nds;
-	m_nds.run();
+	m_nds = std::make_shared<NDS>();
+	std::thread m_workerThread(&emuWorkerThread);
 
+	if(m_workerThread.joinable())
+		m_workerThread.join();
 	return 0;
+}
+
+void emuWorkerThread()
+{
+	Logger::getInstance()->msg(LoggerSeverity::Info, "Entered worker thread!");
+	m_nds->run();
+	Logger::getInstance()->msg(LoggerSeverity::Info, "Exited worker thread!");
 }
