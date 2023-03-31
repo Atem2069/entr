689eb633578e1511b612cc94413b7fbcbc964b1b
Henry Southall <hen0104.hs@gmail.com>
2023-03-31 19:00:23 +0100

    impl simple circular queue for IPCFIFO

--- entr/IPC.h
+++ entr/IPC.h
@@ -2,6 +2,47 @@
 
 #include"Logger.h"
 
+struct IPCFIFO
+{
+	uint32_t lastWord = 0;
+	int front = 0, back = 0;
+	int size = 0;
+	uint32_t buffer[16];
+
+	void push(uint32_t word)
+	{
+		if (size == 16)
+			error = true;
+		buffer[back++] = word;
+		back &= 0xF;	//restrict to [0..15] (circular queue)
+		size++;
+	}
+
+	uint32_t pop()
+	{
+		if (size == 0)
+		{
+			error = true;
+			return lastWord;
+		}
+
+		lastWord = buffer[front++];
+		front &= 0xF;	//restrict to [0..15]
+		size--;
+		return lastWord;
+	}
+
+	void clear()
+	{
+		front = 0;
+		back = 0;
+		size = 0;
+		lastWord = 0;
+	}
+
+	bool error = false;
+};
+
 class IPC
 {
 public:
