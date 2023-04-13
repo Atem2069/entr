150a5951642f04490ab75f75f429f9cb424782e5
Henry Southall <hen0104.hs@gmail.com>
2023-04-13 17:03:28 +0100

    start basic spi touchscreen impl

--- entr/Touchscreen.cpp
+++ entr/Touchscreen.cpp
@@ -2,7 +2,7 @@
 
 Touchscreen::Touchscreen()
 {
-
+	m_data = 0;
 }
 
 Touchscreen::~Touchscreen()
@@ -12,10 +12,38 @@ Touchscreen::~Touchscreen()
 
 uint8_t Touchscreen::sendCommand(uint8_t value)
 {
-	return 0;
+	uint8_t result = ((m_data >> 8) & 0xFF);
+	m_data <<= 8;
+
+	if ((value >> 7) & 0b1)			//update adc y, adc x
+	{
+		adcy = 0xFFF;
+		adcx = 0;
+		if (pressed)
+		{
+			adcy = ((screenY - scry1 + 1) * (adcy2 - adcy1) / (scry2 - scry1) + adcy1) << 3;
+			adcx = ((screenX - scrx1 + 1) * (adcx2 - adcx1) / (scrx2 - scrx1) + adcx1) << 3;
+		}
+	}
+
+	uint8_t channelSelect = ((value >> 4) & 0b111);
+	switch (channelSelect)
+	{
+	case 1:			//y
+		m_data = adcy;
+		break;
+	case 5:			//x
+		m_data = adcx;
+		break;
+	}
+	return result;
 }
 
 void Touchscreen::deselect()
 {
-
+	m_state = TouchscreenState::AwaitCommand;
 }
+
+int Touchscreen::screenX = 0;
+int Touchscreen::screenY = 0;
+bool Touchscreen::pressed = false;
--- entr/Touchscreen.h
+++ entr/Touchscreen.h
@@ -3,6 +3,14 @@
 #include"Logger.h"
 #include"SPIDevice.h"
 
+enum class TouchscreenState
+{
+	AwaitCommand,
+	ReadPositionUpper,
+	ReadPositionLower,
+	Dummy
+};
+
 class Touchscreen : public SPIDevice
 {
 public:
@@ -11,4 +19,13 @@ public:
 
 	uint8_t sendCommand(uint8_t value);
 	void deselect();
+
+	static int screenX;
+	static int screenY;
+	static bool pressed;
+private:
+	TouchscreenState m_state = TouchscreenState::AwaitCommand;
+	uint16_t m_data;
+	int adcx1 = 0, adcx2 = 0, adcy1 = 0, adcy2 = 0, scrx1 = 0, scrx2 = 0, scry1 = 0, scry2 = 0;	//todo: fill out
+	uint16_t adcy = 0, adcx = 0;
 };
