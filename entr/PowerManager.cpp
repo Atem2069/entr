4db4c5f8fd0aa0e94dab02d0c18324203c1587ef
Henry Southall <hen0104.hs@gmail.com>
2023-07-31 18:25:57 +0100

    impl power management device

--- entr/PowerManager.cpp
+++ entr/PowerManager.cpp
@@ -12,10 +12,56 @@ PowerManager::~PowerManager()
 
 uint8_t PowerManager::sendCommand(uint8_t value)
 {
-	return 0;
+	switch (m_state)
+	{
+	case PowerManagerState::Command: return decodeCommand(value);
+	case PowerManagerState::ReadRegister: return readRegister();
+	case PowerManagerState::WriteRegister: return writeRegister(value);
+	}
 }
 
 void PowerManager::deselect()
 {
+	m_state = PowerManagerState::Command;
+}
 
+uint8_t PowerManager::decodeCommand(uint8_t val)
+{
+	registerIdx = val & 0x7F;
+	bool readWrite = (val >> 7) & 0b1;
+	if (readWrite)
+		m_state = PowerManagerState::ReadRegister;
+	else
+		m_state = PowerManagerState::WriteRegister;
+	return 0;
+}
+
+uint8_t PowerManager::readRegister()
+{
+	switch (registerIdx)
+	{
+	case 0: return m_controlReg;
+	case 1: return m_batteryStatus;
+	case 4: return m_backlightControl;
+	default: return 0;
+	}
+}
+
+uint8_t PowerManager::writeRegister(uint8_t val)
+{
+	switch (registerIdx)
+	{
+	case 0:
+	{
+		m_controlReg = val;
+		if ((val >> 6) & 0b1)
+		{
+			Logger::msg(LoggerSeverity::Warn, "Unhandled system shutdown");
+			//should actually shutdown here.. not sure of best approach that isn't hacky.
+		}
+		break;
+	}
+	case 4: m_backlightControl = val; break;
+	}
+	return 0;
 }
--- entr/PowerManager.h
+++ entr/PowerManager.h
@@ -2,6 +2,14 @@
 
 #include"Logger.h"
 #include"SPIDevice.h"
+#include"Config.h"
+
+enum class PowerManagerState
+{
+	Command,
+	ReadRegister,
+	WriteRegister
+};
 
 class PowerManager : public SPIDevice
 {
@@ -11,4 +19,16 @@ public:
 
 	uint8_t sendCommand(uint8_t value);
 	void deselect();
+
+private:
+	PowerManagerState m_state = PowerManagerState::Command;
+	uint8_t registerIdx = {};
+
+	uint8_t decodeCommand(uint8_t val);
+	uint8_t readRegister();
+	uint8_t writeRegister(uint8_t val);
+
+	uint8_t m_controlReg = 0;
+	uint8_t m_batteryStatus = 1;
+	uint8_t m_backlightControl = 0;
 };
